6d47aba588dc1206fd0efa16a7160275192c3344
user <sayinmehmet47@gmail.com>
2022-03-19 11:26:11 +0100

    environment variable added

--- admin/dataProvider.js
+++ admin/dataProvider.js
@@ -1,22 +1,22 @@
-import { fetchUtils } from 'react-admin';
+import { fetchUtils } from "react-admin";
 
-import { stringify } from 'query-string';
+import { stringify } from "query-string";
 const httpClient = fetchUtils.fetchJson;
 
-
 const dataProvider = {
-    getList: (resource, params) => {
-        const { page, perPage } = params.pagination;
-        const query = {
-            range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
-        };
-        const url = `/api/${resource}?${stringify(query)}`;
-        return httpClient(url).then(({ headers, json }) => ({
-           data: json.map(resource => ({ ...resource, id: resource._id }) ),
-            total: parseInt(headers.get('content-range').split('/').pop(), 10),
-        }));
-    },
-
+  getList: (resource, params) => {
+    const { page, perPage } = params.pagination;
+    const query = {
+      range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
+    };
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/${resource}?${stringify(
+      query
+    )}`;
+    return httpClient(url).then(({ headers, json }) => ({
+      data: json.map((resource) => ({ ...resource, id: resource._id })),
+      total: parseInt(headers.get("content-range").split("/").pop(), 10),
+    }));
+  },
 };
 
-export default dataProvider
+export default dataProvider;
