dc0629d8879b6b6474e95b9f597593e3fd3fd330
user <sayinmehmet47@gmail.com>
2022-03-26 10:55:43 +0100

    create api added

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -26,10 +26,30 @@ export async function getData() {
   const products = await Products.find()
   return products
 }
+export async function createData(item) {
+  const products = await Products.create(item)
+  return products
+}
 
 export default async function handler(req, res) {
     const { method } = req;
   await dbConnect();
-  const products = await getData()
-  res.status(200).json(products)
+  if(method==="GET"){
+    try {
+      
+      const products = await getData()
+      res.status(200).json(products)
+    } catch (error) {
+      res.status(500).json(err);
+    }
+  }
+  if(method==="POST"){
+    try {
+      
+      const products = await createData(req.body)
+      res.status(200).json(products)
+    } catch (error) {
+      res.status(500).json(err);
+    }
+  }
 }
